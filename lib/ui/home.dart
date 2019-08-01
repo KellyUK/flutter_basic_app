1be76f0c21988e4728f449fea92e34254a2bd94e
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-08-01 16:47:25 +0100

    added pizza image

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,60 +5,73 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(top: 30.0, left: 10.0),
-            color: Colors.deepOrangeAccent,
-            child: Column(
-              children: <Widget>[
-                Row(children: <Widget>[
-                  Text(
-                    "Ham and Mushroom",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      decoration: TextDecoration.none,
-                      fontFamily: 'Oxygen',
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      "Onions, chilli, beef, pepperoni, peppers",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        decoration: TextDecoration.none,
-                        fontFamily: 'Oxygen',
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ),
-                ]),
-                Row(children: <Widget>[
-                  Text(
-                    "Spicy",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      decoration: TextDecoration.none,
-                      fontFamily: 'Oxygen',
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      "Ham, mushrooms, onions",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        decoration: TextDecoration.none,
-                        fontFamily: 'Oxygen',
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ),
-                ])
-              ],
-            )));
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(30.0),
+      color: Colors.deepOrangeAccent,
+      child: Column(children: <Widget>[
+        Row(children: <Widget>[
+          Text(
+            "Ham and Mush",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              decoration: TextDecoration.none,
+              fontFamily: 'Oxygen',
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              "Ham, mushrooms, onions",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 15.0,
+                decoration: TextDecoration.none,
+                fontFamily: 'Oxygen',
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        ]),
+        Row(children: <Widget>[
+          Text(
+            "Hot and Spicy",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              decoration: TextDecoration.none,
+              fontFamily: 'Oxygen',
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              "Onions, chilli, peppers, beef, pepperoni",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 15.0,
+                decoration: TextDecoration.none,
+                fontFamily: 'Oxygen',
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        ]),
+        PizzaImageWidget(),
+      ]),
+    ));
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = AssetImage('images/pizza-clip-art-31.png');
+    Image image = Image(
+      image: pizzaAsset,
+      width: 400.0,
+      height: 400.0,
+    );
+    return Container(child: image);
   }
 }
