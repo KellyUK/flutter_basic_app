c401ccce3e0bec2ad75d8dd6bd0a19a58e5c5b09
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-08-01 12:08:25 +0100

    added Center container

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,6 +3,14 @@
  class Home extends StatelessWidget{
    @override
    Widget build(BuildContext context) {
-     return Container();
+     return Center(
+       child:Container(
+       alignment: Alignment.center,
+       color: Colors.deepOrangeAccent,
+       width: 192,
+       height: 192,
+       child: Text("Pizza", textDirection: TextDirection.ltr ,),
+       )
+     );
    }
  }
